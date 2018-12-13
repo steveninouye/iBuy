5ac31bd2d5b97bb711c8b5586bea9a4d5db8c849
Steven Inouye <steveninouye@msn.com>
2018-12-13 10:43:52 -0800

    Fix session form width to take up full width

---
+++ src/components/homepage/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = (props) => (
+   <div className="home--footer">
+      <div className="home--footer-disclaimer" />
+   </div>
+);
+
+export default Footer;
