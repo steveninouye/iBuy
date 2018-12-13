0efc79040d14df821d1ab15947418bb78ad10fe8
Steven Inouye <steveninouye@msn.com>
2018-12-12 16:20:23 -0800

    add images to webpack assets

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
    entry: './src/App.jsx',
    output: {
       path: path.join(__dirname, 'app', 'assets', 'javascripts'),
-      filename: 'bundle.js'
+      filename: 'bundle.js',
+      publicPath: '/app/assets'
    },
    module: {
       rules: [
