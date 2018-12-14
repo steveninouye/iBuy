6806ca64307f95d61e7d79b32013f2dff74501b2
Steven Inouye <steveninouye@msn.com>
2018-12-14 08:48:04 -0800

    add box shadow to reacently viewed items

--- src/components/homepage/Home.jsx
+++ src/components/homepage/Home.jsx
@@ -18,6 +18,7 @@ class Home extends React.Component {
                <HomeBanner />
                <RecentlyViewedItems />
                <CategoryBars />
+               
             </div>
          </>
       );
