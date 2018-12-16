665e9877f35f49d5932142cbde8c707114ea2bd5
Steven Inouye <steveninouye@msn.com>
2018-12-16 10:24:54 -0800

    Complete Search Result Item view

--- src/components/productResults/SearchResultItem.jsx
+++ src/components/productResults/SearchResultItem.jsx
@@ -1,15 +1,51 @@
 import React from 'react';
 
+import {
+   randomStarRating,
+   randNumStarRating,
+   randNum
+} from '../../utils/data_conversion_utils';
+
 const SearchResultItem = ({
-   product: { description, title, sell_by, buy_it_now, photos }
-}) => (
-   <div className="search-result-item">
-      <div className="thumbnail">
-         <img src={photos[0]} />
+   product: { description, title, sellBy, buyItNow, photos }
+}) => {
+   console.log(typeof sellBy);
+   buyItNow = !buyItNow ? (
+      undefined
+   ) : (
+      <>
+         <div className="buy-it-now-price">{`$${(buyItNow / 100).toFixed(
+            2
+         )}`}</div>
+         <div className="buy-it-now">Buy It Now</div>
+      </>
+   );
+   return (
+      <div className="search-result-item">
+         <div className="thumbnail">
+            <img src={photos[0]} />
+         </div>
+         <div className="item-title">{title}</div>
+         <div className="item-subtitle">
+            {description
+               .slice(0, 45)
+               .split('<br>')
+               .join(' ')}
+         </div>
+         <div className="star-rating">
+            {randomStarRating()}
+            <span className="num-star-rating">
+               {randNumStarRating()} product ratings
+            </span>
+         </div>
+         <div className="item-price">
+            <div className="current-price">$17.50</div>
+            <div className="item-num-bids">{`${randNum(2, 15)} bids`}</div>
+            {buyItNow}
+         </div>
+         <div className="sell-by">4d 21h</div>
       </div>
-      <div className="item-title">{title}</div>
-      <div className="item-subtitle">{description.slice(0, 20)}</div>
-   </div>
-);
+   );
+};
 
 export default SearchResultItem;
--- src/utils/data_conversion_utils.js
+++ src/utils/data_conversion_utils.js
@@ -6,3 +6,66 @@ export const translateLineBreaks = (text) =>
            .split('<br>')
            .reduce((prev, curr, i) => [...prev, <br key={i} />, curr])
       : undefined;
+
+export const randomStarRating = () => {
+   const rating = Math.floor(Math.random() * 5);
+   switch (rating) {
+      case 0:
+         return (
+            <>
+               <i className="fas fa-star" />
+               <i className="fas fa-star" />
+               <i className="fas fa-star" />
+               <i className="far fa-star" />
+               <i className="far fa-star" />
+            </>
+         );
+      case 1:
+         return (
+            <>
+               <i className="fas fa-star" />
+               <i className="fas fa-star" />
+               <i className="fas fa-star" />
+               <i className="fas fa-star-half-alt" />
+               <i className="far fa-star" />
+            </>
+         );
+      case 2:
+         return (
+            <>
+               <i className="fas fa-star" />
+               <i className="fas fa-star" />
+               <i className="fas fa-star" />
+               <i className="fas fa-star" />
+               <i className="far fa-star" />
+            </>
+         );
+      case 3:
+         return (
+            <>
+               <i className="fas fa-star" />
+               <i className="fas fa-star" />
+               <i className="fas fa-star" />
+               <i className="fas fa-star" />
+               <i className="fas fa-star-half-alt" />
+            </>
+         );
+      case 4:
+         return (
+            <>
+               <i className="fas fa-star" />
+               <i className="fas fa-star" />
+               <i className="fas fa-star" />
+               <i className="fas fa-star" />
+               <i className="fas fa-star" />
+            </>
+         );
+   }
+};
+
+export const randNumStarRating = () => 20 + Math.floor(Math.random() * 200);
+
+export const randNum = (min, max) => {
+   const spread = max - min;
+   return min + Math.floor(Math.random() * spread);
+};
