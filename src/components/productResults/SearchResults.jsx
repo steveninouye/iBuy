947e17ae8453fe5d7b830624a3893995091378f3
Steven Inouye <steveninouye@msn.com>
2018-12-17 14:27:53 -0800

    Add ebay loader to search results

--- src/components/productResults/SearchResults.jsx
+++ src/components/productResults/SearchResults.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import queryString from 'query-string';
+import { PropagateLoader } from 'react-spinners';
 
 import SearchResultItem from './SearchResultItem';
 
@@ -7,7 +8,8 @@ class SearchResults extends React.Component {
    constructor(props) {
       super(props);
       this.state = {
-         queryString: this.props.location.search
+         queryString: this.props.location.search,
+         loading: true
       };
    }
 
@@ -18,9 +20,11 @@ class SearchResults extends React.Component {
    componentDidUpdate(prevProps, prevState, snapshot) {
       const queryString = this.props.location.search;
       if (this.state.queryString !== queryString) {
-         this.setState({ queryString }, () => {
+         this.setState({ queryString, loading: true }, () => {
             this.props.searchProducts(this.parsedQueryString());
          });
+      } else if (this.state.loading) {
+         this.setState({ loading: false });
       }
    }
 
@@ -30,7 +34,18 @@ class SearchResults extends React.Component {
 
    render() {
       const { products } = this.props;
-      if (products.length > 0) {
+      if (this.state.loading) {
+         return (
+            <ul id="search-results">
+               <PropagateLoader
+                  sizeUnit={'px'}
+                  size={15}
+                  color={'#000000'}
+                  loading={this.state.loading}
+               />
+            </ul>
+         );
+      } else if (products.length > 0) {
          return (
             <ul id="search-results">
                {products.map((product, i) => (
@@ -39,7 +54,11 @@ class SearchResults extends React.Component {
             </ul>
          );
       } else {
-         return <ul id="search-results">spinner</ul>;
+         return (
+            <ul id="search-results">
+               <li>No Results found</li>
+            </ul>
+         );
       }
    }
 }
