aa5908ece12b4c8323d386ff0fa15a9537ed6fe1
Steven Inouye <steveninouye@msn.com>
2018-12-18 10:03:47 -0800

    Push to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:index, :show, :create, :destroy, :update]
+    resources :products, only: [:index, :show, :create, :update, :destroy]
+    resource :session, only: [:create, :destroy]
+  end
+
+  root "static_pages#root"
 end
