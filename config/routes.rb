a7b15d356462226b8a9c80066b44f3985a12ffef
Steven Inouye <steveninouye@msn.com>
2018-12-18 09:40:09 -0800

    Remove extra images and refreshed config directory

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  namespace :api, defaults:{format: :json}  do
-    resources :users, only: [:index, :show, :create, :destroy, :update]
-    resources :products, only: [:index, :show, :create, :update, :destroy]
-    resource :session, only: [:create, :destroy]
-  end
-  
-  root "static_pages#root"
-
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
