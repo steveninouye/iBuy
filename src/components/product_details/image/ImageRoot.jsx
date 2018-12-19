9af4d1bb9374e59d63e04f1804f20e87ba354884
Steven Inouye <steveninouye@msn.com>
2018-12-19 11:18:03 -0800

    Complete styling of item details picture section

--- src/components/product_details/image/ImageRoot.jsx
+++ src/components/product_details/image/ImageRoot.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 const ImageRoot = props => (
    <div className="item-details-image-root">
+      <div className="main-image">
+      
+      </div>
+      <div className="image-selector-container">
+      <a className="image-left-chevron"></a>
+      <div className="image-1"></div>
+      <div className="image-2"></div>
+      <div className="image-3"></div>
+      <div className="image-4"></div>
+      <div className="image-5"></div>
+      <div className="image-6"></div>
+      <a className="image-right-chevron"></a>
+      </div>
    </div>
 )
 
--- src/components/product_details/payment/PaymentRoot.jsx
+++ src/components/product_details/payment/PaymentRoot.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-const PaymentRoot = props => (
-   <div className="item-details-payment-root"></div>
-)
+const PaymentRoot = (props) => (
+   <div className="item-details-payment-root">
+      <div className="item-details-title">
+         Apple MacBook Air 13" Laptop - Z05J00006
+      </div>
+      <div className="item-details-views-per-hour">
+         <img src="https://s3-us-west-1.amazonaws.com/ibuy-public/fire-icon.png" />
+         1 view per hour
+      </div>
+      <div className="item-details-rating" />
+   </div>
+);
 
 export default PaymentRoot;
