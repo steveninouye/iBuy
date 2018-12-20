1607cca3be1a08bfe7857140208202e7b8b2250d
Steven Inouye <steveninouye@msn.com>
2018-12-19 16:48:50 -0800

    Style Bid/Buy it Now Form

--- src/components/product_details/payment/PaymentForm.jsx
+++ src/components/product_details/payment/PaymentForm.jsx
@@ -21,16 +21,43 @@ class PaymentForm extends React.Component {
    render() {
       return (
          <form onSubmit={this.handleSubmit}>
-            <label>
-               <span className="starting-bid-tag">Starting bid</span>
-               <span className="starting-bid-price">US $385.00</span>
-               <input type="text" onChange={this.handleChange('bid')} />
-            </label>
-            <input type="submit" value="Place bid" />
+            <div className="top-form-section">
+               <label for="bid-input" className="starting-bid-tag">
+                  Starting bid:
+               </label>
+               <div className="top-middle-form-section">
+                  <label for="bid-input" className="starting-bid-price">
+                     US $385.00
+                  </label>
+                  <input
+                     id="bid-input"
+                     type="text"
+                     onChange={this.handleChange('bid')}
+                  />
+                  <div className="min-bid-price">Enter US $385.00 or more</div>
+               </div>
+               <div className="top-right-form-section">
+                  <div className="form-num-bids">
+                     [ <span className="form-num-bids">10 bids</span> ]
+                  </div>
+                  <input type="submit" value="Place bid" />
+               </div>
+            </div>
             <div className="price-hr-divider" />
-            Price: US $900.00<button className="buy-it-now-btn">Buy It Now</button>
-            <button className="add-to-cart-btn">Add to cart</button>
-            <a href="#" className="add-to-watch-list-btn">Add to watch list</a>
+            <div className="bottom-form-section">
+               <div className="left-bottom-buy-it-now">Price:</div>
+               <div className="middle-bottom-buy-it-now">US $900.00</div>
+               <div className="right-bottom-buy-it-now">
+                  <button className="buy-it-now-btn">Buy It Now</button>
+                  <button className="add-to-cart-btn">Add to cart</button>
+                  <div className="form-add-to-watch-btn">
+                     <a href="#" className="add-to-watch-list-btn">
+                        Add to watch list
+                     </a>
+                     <a href="#" className="add-to-watch-list-carrot" />
+                  </div>
+               </div>
+            </div>
          </form>
       );
    }
