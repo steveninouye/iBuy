757ff9361d9aaae61a9192079e19605215c0c9a5
Steven Inouye <steveninouye@msn.com>
2018-12-20 09:07:24 -0800

    Complete styling of the show page

--- src/components/product_details/description/DescriptionRoot.jsx
+++ src/components/product_details/description/DescriptionRoot.jsx
@@ -1,8 +1,67 @@
-import React from 'react'
+import React from 'react';
 
-const DescriptionRoot = props => (
+const DescriptionRoot = (props) => (
    <div className="item-details-description-root">
+      <ul className="item-details-body-tabs">
+         <li className="description-tag">Description</li>
+         <li className="shipping-payments-tag">Shipping and payments</li>
+         <li className="spacer" />
+         <li className="report-item">Report Item</li>
+      </ul>
+      <div className="description-body">
+         Selling manufacture refurbished Lenovo Thinkpad P1 Mobile Workstation.
+         Item is sealed in the box and carry 1 year Lenovo warranty.
+         <br />
+         <br />
+         Sepcs:
+         <br />
+         Processor : 8th Generation Intel® Core™ i7-8850H vPro 6 Core Processor
+         (2.60GHz, up to 4.30GHz with Turbo Boost, 9MB Cache)
+         <br />
+         Operating System : Windows 10 Home 64
+         <br />
+         Operating System Language : Windows 10 Home 64 English
+         <br />
+         Display : 15.6" 4K UHD (3840 x 2160) IPS multi-touch, anti-reflective /
+         anti-smudge, 400 nits
+         <br />
+         Memory : 16 GB DDR4 2666MHz
+         <br />
+         Graphic Card : Nvidia Quadro P2000 4GB
+         <br />
+         Camera : IR & 720p HD Camera with microphone
+         <br />
+         Keyboard : Backlit Keyboard - US English
+         <br />
+         Fingerprint Reader : Fingerprint Reader
+         <br />
+         Security Chip : Hardware dTPM
+         <br />
+         First Hard Drive : 512GB Solid State Drive PCIe-NVMe OPAL2.0 M.2
+         <br />
+         Total Usable Storage Capacity : 512GB
+         <br />
+         Battery : 4 cell Li-Polymer 80Wh
+         <br />
+         Power Cord : 135W AC Adapter (2pin)
+         <br />
+         Wireless : Intel 9560 802.11AC vPro (2 x 2) & Bluetooth 5.0
+         <br />
+         Ethernet Dongle : Full RJ45 Gigabit Ethernet Extension Adapter
+         <br />
+         vPro Certified Model : vPro Certified
+         <br />
+         Display Shell : 15.6" UHD (3840 x 2160), IPS, Multi-Touch, with
+         IR-Camera, with Mic, with WLAN
+         <br />
+         Package Box : Standard Packaging
+         <br />
+         Language Pack : Publication - English
+         <br />
+         Warranty : 1 Year Depot or Carry-in
+         <br />
+      </div>
    </div>
-)
+);
 
 export default DescriptionRoot;
