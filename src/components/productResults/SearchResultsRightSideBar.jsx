4bc713b6a388b2500e4cde77172a7f0edfe59be8
Steven Inouye <steveninouye@msn.com>
2019-01-13 18:33:50 -0800

    Move images into public directory and referenced home page images to coorelating images

--- src/components/productResults/SearchResultsRightSideBar.jsx
+++ src/components/productResults/SearchResultsRightSideBar.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 
 const SearchResultsRightSideBar = (props) => (
-   <div className="right-side-bar">
-      <a href="https://www.appacademy.io/" className="advertisement">
-         <img
-            src="https://s3-us-west-1.amazonaws.com/ibuy-public/aa-logo.jpg"
-            alt="App Academy Logo"
-         />
-         Developing the Best Software Engineers
-      </a>
-   </div>
+  <div className="right-side-bar">
+    <a href="https://www.appacademy.io/" className="advertisement">
+      <img src="/images/aa-logo.jpg" alt="App Academy Logo" />
+      Developing the Best Software Engineers
+    </a>
+  </div>
 );
 
 export default SearchResultsRightSideBar;
