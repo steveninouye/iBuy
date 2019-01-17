f151ef4eaec61a8467727997becae1eeec121dcb
Steven Inouye <steveninouye@msn.com>
2019-01-16 21:57:20 -0800

    Got all links on search result page

--- scraper.rb
+++ scraper.rb
@@ -1,61 +1,66 @@
-require 'httparty'
-require 'nokogiri'
+require "httparty"
+require "nokogiri"
 
-class Scraper
-
-  def initialize
-    @URL = "https://example.com"
-    @JOBS = []
-  end
-
-  def get_all_data
-    num = 1
-    page = scrape_page(@URL)
-    per_page = page.css('div.listingCard').count
-    total = set_total(page)
-    last_page = set_last_page(total, per_page)
-    loop_pages(num, last_page)
-  end
-
-  def scrape_page(url)
-    scrape = Nokogiri::HTML(HTTParty.get(url))
-  end
-
-  def set_total(data)
-    data.css('div.job-count').text.split(' ')[1].gsub(',','').to_i
-  end
-
-  def set_last_page(total, per_page)
-    (total.to_f/per_page.to_f).round
-  end
-
-  def loop_pages(num, last_page)
-    while num <= last_page
-      handle_pagination(num)
-      num += 1
-    end
-  end
-
-  def handle_pagination(num)
-    url = "#{@URL}/listings?page=#{num}"
-    jobs = scrape_page(url).css('div.listingCard')
-    build_jobs(jobs)
-  end
-
-  def build_jobs(jobs)
-    jobs.each do |j|
-	    job = build_job(j)
-	    @JOBS << job
-    end
-  end
-
-  def build_job(job)
-    resutl = {
-      title: job.css('span.job-title').text,
-      company: job.css('span.company').text,
-      location: job.css('span.location').text,
-      url: "#{@URL}/#{job.css('a')[0].attributes['href'].value}"
-    }
-    result
-  end
+page = Nokogiri::HTML(HTTParty.get("https://sfbay.craigslist.org/d/computers/search/sya"))
+page.css("li.result-row > a").each do |link|
+  p link.attributes["href"].value
 end
+
+# class Scraper
+
+#   def initialize
+#     @URL = "https://example.com"
+#     @JOBS = []
+#   end
+
+#   def get_all_data
+#     num = 1
+#     page = scrape_page(@URL)
+#     per_page = page.css('div.listingCard').count
+#     total = set_total(page)
+#     last_page = set_last_page(total, per_page)
+#     loop_pages(num, last_page)
+#   end
+
+#   def scrape_page(url)
+#     scrape = Nokogiri::HTML(HTTParty.get(url))
+#   end
+
+#   def set_total(data)
+#     data.css('div.job-count').text.split(' ')[1].gsub(',','').to_i
+#   end
+
+#   def set_last_page(total, per_page)
+#     (total.to_f/per_page.to_f).round
+#   end
+
+#   def loop_pages(num, last_page)
+#     while num <= last_page
+#       handle_pagination(num)
+#       num += 1
+#     end
+#   end
+
+#   def handle_pagination(num)
+#     url = "#{@URL}/listings?page=#{num}"
+#     jobs = scrape_page(url).css('div.listingCard')
+#     build_jobs(jobs)
+#   end
+
+#   def build_jobs(jobs)
+#     jobs.each do |j|
+# 	    job = build_job(j)
+# 	    @JOBS << job
+#     end
+#   end
+
+#   def build_job(job)
+#     resutl = {
+#       title: job.css('span.job-title').text,
+#       company: job.css('span.company').text,
+#       location: job.css('span.location').text,
+#       url: "#{@URL}/#{job.css('a')[0].attributes['href'].value}"
+#     }
+#     result
+#   end
+# end
